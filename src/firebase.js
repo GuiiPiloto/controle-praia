605c293176c4ebe08aca54725332a6c2e586a43c
GuilhermePiloto-tech <guilherme.piloto@denvercontabil.com.br>
2026-04-15 21:24:32 -0300

    Atualização Página

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBRZtW7N2LRjRAlPVntciHf9rf_0AWY8E",
-  authDomain: "controle-praia.firebaseapp.com",
-  projectId: "controle-praia",
-  storageBucket: "controle-praia.firebasestorage.app",
-  messagingSenderId: "1007327676976",
-  appId: "1:1007327676976:web:57d3f0ec7c496a0a0f3bf5"
+  apiKey: "AIzaSyDlKU2D66-etmOHtlefViHvx4whRVdRiEE",
+  authDomain: "controle-praia-2026.firebaseapp.com",
+  projectId: "controle-praia-2026",
+  storageBucket: "controle-praia-2026.firebasestorage.app",
+  messagingSenderId: "744354320192",
+  appId: "1:744354320192:web:da72b446d481158ce834a5"
 };
 
 const app = initializeApp(firebaseConfig);
